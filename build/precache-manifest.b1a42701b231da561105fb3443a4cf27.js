ad6842fabc3f53b3aca482a3c19148919436b00b
Marta Font <martafontb@gmail.com>
2020-10-14 20:25:23 +0200

    untracked

---
+++ build/precache-manifest.b1a42701b231da561105fb3443a4cf27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4764562618348619e642fbb262bbcd1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1404b3f6f0aa0cf9c2",
+    "url": "/static/css/main.ff47908f.chunk.css"
+  },
+  {
+    "revision": "1e6b4d4912ee7a590fdc",
+    "url": "/static/js/2.e8e8d98d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8e8d98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1404b3f6f0aa0cf9c2",
+    "url": "/static/js/main.326542ec.chunk.js"
+  },
+  {
+    "revision": "c98071a593d5dd28c911",
+    "url": "/static/js/runtime-main.b413768e.js"
+  },
+  {
+    "revision": "5b9a3ae1255e28dc936234fe3c03e06c",
+    "url": "/static/media/close-icon.5b9a3ae1.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/static/media/loader.be00fc4a.svg"
+  }
+]);
