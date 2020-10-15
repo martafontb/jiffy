23f7839ffae69d9f5d9b0345edece41e4b18c5e6
Marta Font <martafontb@gmail.com>
2020-10-15 10:57:39 +0200

    yarn uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 //here we import in our loader spinner image
 import loader from './images/loader.svg';
-
 import clearButton from './images/close-icon.svg';
-
 import Gif from './Gif';
 
 
